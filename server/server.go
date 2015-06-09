9e0531caab4f0d178986927f7863bdbb0a685a75
Steve Mostovoy <stevemostovoysm@gmail.com>
2015-06-08 21:42:06 -0700

    Add flag parsing, remove debug line.

--- server/server.go
+++ server/server.go
@@ -8,16 +8,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var hostname = flag.String("hostname", "localhost", "The hostname - defaults to localhost")
+var hostname = flag.String("hostname", "localhost", "The hostname to use for routing.")
 
 func Serve() {
-	fmt.Println("Starting webserver on :80")
-	fmt.Println(*hostname)
+	flag.Parse()
+
 	r := mux.NewRouter()
 	s := r.Host(*hostname).Subrouter()
 	s.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	s = r.Host("blog." + *hostname).Subrouter()
 	s.PathPrefix("/").Handler(http.FileServer(http.Dir("./blog/static/")))
 	http.Handle("/", r)
+
+	fmt.Println("Starting webserver on :80")
 	http.ListenAndServe(":80", nil)
 }
