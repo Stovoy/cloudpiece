71e9541bac5926d715a42d3e061621c9044a7466
Steve Mostovoy <stevemostovoysm@gmail.com>
2015-06-08 21:39:52 -0700

    Add hostname flag.

--- server/server.go
+++ server/server.go
@@ -3,16 +3,20 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"flag"
 
 	"github.com/gorilla/mux"
 )
 
+var hostname = flag.String("hostname", "localhost", "The hostname - defaults to localhost")
+
 func Serve() {
 	fmt.Println("Starting webserver on :80")
+	fmt.Println(*hostname)
 	r := mux.NewRouter()
-	s := r.Host("localhost").Subrouter()
+	s := r.Host(*hostname).Subrouter()
 	s.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-	s = r.Host("blog.localhost").Subrouter()
+	s = r.Host("blog." + *hostname).Subrouter()
 	s.PathPrefix("/").Handler(http.FileServer(http.Dir("./blog/static/")))
 	http.Handle("/", r)
 	http.ListenAndServe(":80", nil)
